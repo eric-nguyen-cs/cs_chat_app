5d2f18891073937661316635b656fd1a4bfe0b42
Eric Nguyen <ericnguyen.developper@gmail.com>
2024-03-12 17:13:54 +0100

    feat: add form

--- next-frontend/src/app/chat/AddMessage.tsx
+++ next-frontend/src/app/chat/AddMessage.tsx
@@ -1,11 +1,54 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const newMessageSchema = z.object({
+  newMessage: z.string().trim().min(1),
+});
 
 export function AddMessage() {
+  const form = useForm<z.infer<typeof newMessageSchema>>({
+    resolver: zodResolver(newMessageSchema),
+    defaultValues: {
+      newMessage: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof newMessageSchema>) => {
+    console.log(values);
+  };
+
   return (
-    <div className="flex flex-col w-full gap-2">
-      <Textarea placeholder="Type your message here." />
-      <Button className="self-end">Send message</Button>
-    </div>
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col w-full gap-2"
+      >
+        <FormField
+          control={form.control}
+          name="newMessage"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Textarea placeholder="Type your message here." {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <Button
+          className="self-end"
+          type="submit"
+          disabled={!form.formState.isValid}
+        >
+          Send message
+        </Button>
+      </form>
+    </Form>
   );
 }
