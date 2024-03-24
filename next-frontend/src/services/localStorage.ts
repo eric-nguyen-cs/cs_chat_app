b5e8b15f4b002d3475494be19f98b59547605d88
Eric Nguyen <ericnguyen.developper@gmail.com>
2024-03-24 16:23:06 +0100

    fix: local storage error

--- next-frontend/src/services/localStorage.ts
+++ next-frontend/src/services/localStorage.ts
@@ -1,19 +1,30 @@
-type LocalItemKey = 'token' | 'username';
+"use client";
+
+type LocalItemKey = "token" | "username";
 class LocalItem {
   key: LocalItemKey;
   public constructor(key: LocalItemKey) {
     this.key = key;
   }
   get() {
+    if (typeof window === "undefined") {
+      return null;
+    }
     return localStorage.getItem(this.key);
   }
   set(data: string) {
+    if (typeof window === "undefined") {
+      return;
+    }
     localStorage.setItem(this.key, data);
   }
   reset() {
+    if (typeof window === "undefined") {
+      return;
+    }
     localStorage.removeItem(this.key);
   }
 }
 
-export const LocalToken = new LocalItem('token');
-export const LocalUsername = new LocalItem('username');
+export const LocalToken = new LocalItem("token");
+export const LocalUsername = new LocalItem("username");
